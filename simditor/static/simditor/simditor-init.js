90bc83a0e7a131c526cec387de8f545ddc03c866
istommao <istommao@gmail.com>
2017-06-09 21:40:30 +0800

    Add CHANGELOG.md and tox.ini

--- simditor/static/simditor/simditor-init.js
+++ simditor/static/simditor/simditor-init.js
@@ -8,6 +8,9 @@
 
     var $ = jQuery || djangoJQuery;
     $(function() {
+        var toolbar = ['title', 'bold', 'italic', 'underline', 'strikethrough', 'fontScale', 'color', '|',
+                       'ol', 'ul', 'blockquote', 'code', 'table', '|', 'link', 'image', 'hr', '|',
+                       'indent', 'outdent', 'alignment',  'checklist', 'markdown', 'fullscreen'];
         var editor = new Simditor({
             textarea: $('#editor'),
             upload: {
--- simditor_demo/app/admin.py
+++ simditor_demo/app/admin.py
@@ -3,11 +3,7 @@ from django.contrib import admin
 
 from app.models import News
 
-from app.forms import SimditorForm
-
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     """NewsAdmin."""
-
-    form = SimditorForm
